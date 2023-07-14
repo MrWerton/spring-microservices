27571e2664a5dd85bf16cafa41c8578d7f92ba82
Werton <mr.werton@gmail.com>
2023-07-14 14:19:05 -0300

    feat: added api gateway

--- serviceB/src/main/java/com/notrew/serviceb/Controller.java
+++ serviceB/src/main/java/com/notrew/serviceb/Controller.java
@@ -1,4 +1,4 @@
-package com.notrew.servicea;
+package com.notrew.serviceb;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/servicea")
+@RequestMapping("/serviceb")
 public class Controller {
     @GetMapping
-    public  ResponseEntity<String> getHello(){
-        return ResponseEntity.ok("Hello, service A");
+    public ResponseEntity<String> getHello(){
+        return ResponseEntity.ok("Hello, service B");
     }
 }
---
+++ serviceB/src/main/java/com/notrew/serviceb/ServiceBApplication.java
@@ -0,0 +1,19 @@
+package com.notrew.serviceb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+public class ServiceBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceBApplication.class, args);
+    }
+
+
+}
